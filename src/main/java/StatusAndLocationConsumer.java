fdb1e00f50cd336e0ce827c1ebbaad374f33dd54
kalyssao <klysssa@icloud.com>
2021-11-10 12:37:32 +0000

    renamed & added general producer/consumer activity

--- src/main/java/StatusAndLocationConsumer.java
+++ src/main/java/StatusAndLocationConsumer.java
@@ -1,5 +1,37 @@
-public class StatusConsumer {
-    public static void main(String[] args) {
+import org.apache.kafka.clients.consumer.Consumer;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.Consumer;
 
+import java.nio.file.Paths;
+import java.time.Duration;
+import java.util.Collections;
+
+public class StatusAndLocationConsumer {
+
+    private final Consumer<String, String> consumer;
+
+
+    public StatusAndLocationConsumer(final Consumer<String, String> consumer,
+                                     final ConsumerRecordsHandler<String, String> recordsHandler) {
+        this.consumer = consumer;
+    }
+
+    public static void main(String[] args) {
+        final Consumer<String, String> consumer = new StatusAndLocationConsumer();
+        consumer.subscribe(Collections.singletonList("statusAndLocations"));
+
+        final String filePath = "";
+
+        try {
+
+            while (true) {
+                ConsumerRecords records = consumer.poll(Duration.ofSeconds(1));
+                FileWritingRecordsHandler handler = new FileWritingRecordsHandler(Paths.get(filePath));
+
+                handler.process(records);
+            }
+        } finally {
+            consumer.close();
+        }
     }
 }
--- src/main/java/StatusAndLocationProducer.java
+++ src/main/java/StatusAndLocationProducer.java
@@ -1,5 +1,49 @@
-public class StatusProducer {
-    public static void main(String[] args) {
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.concurrent.Future;
+import java.util.stream.Collectors;
+
+public class StatusAndLocationProducer {
+
+    private final Producer<String, String> producer;
+    final String outTopic;
+
+    public StatusAndLocationProducer(final Producer<String, String> producer, final String topic){
+        this.producer = producer;
+        outTopic = topic;
+    }
+
+    // Given a status message we produce a record
+    public Future<RecordMetadata> produce(final String message) {
+        final String key = "123";
+        final String value;
+
+        // parse the key and
+
+        final ProducerRecord<String, String> producerRecord = new ProducerRecord<>(outTopic, key);
+
+        return producer.send(producerRecord);
+    }
+
+    public static void main(String[] args) {
+        final Producer<String, String> producer = new StatusAndLocationProducer();
+
+        String filePath = "";
+
+        try {
+            List<String> linesToProduce = Files.readAllLines(Paths.get(filePath));
+            List<Future<RecordMetadata>> metadata = linesToProduce.stream()
+                    .map(producer::produce)
+                    .collect(Collectors.toList());
+
+        } catch (IOException e) {
+            System.err.printf("Error reading file %s due to %s %n", filePath, e);
+        }
     }
 }
