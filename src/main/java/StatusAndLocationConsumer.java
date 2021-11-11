3df1dfadaf39d612b255520eec1e2c176f9b6f30
kalyssao <klysssa@icloud.com>
2021-11-11 15:03:31 +0000

    updated gitignore, finished consumer and producer classes

--- src/main/java/StatusAndLocationConsumer.java
+++ src/main/java/StatusAndLocationConsumer.java
@@ -1,37 +1,55 @@
 import org.apache.kafka.clients.consumer.Consumer;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.Consumer;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
 
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.nio.file.Paths;
 import java.time.Duration;
 import java.util.Collections;
+import java.util.Properties;
 
 public class StatusAndLocationConsumer {
-
     private final Consumer<String, String> consumer;
-
+    private final ConsumerRecordsHandler recordsHandler;
 
     public StatusAndLocationConsumer(final Consumer<String, String> consumer,
                                      final ConsumerRecordsHandler<String, String> recordsHandler) {
         this.consumer = consumer;
+        this.recordsHandler = recordsHandler;
     }
 
-    public static void main(String[] args) {
-        final Consumer<String, String> consumer = new StatusAndLocationConsumer();
+    public static Properties loadProperties(String filename) throws IOException {
+        final Properties props = new Properties();
+        final FileInputStream input = new FileInputStream(filename);
+
+        props.load(input);
+        input.close();
+
+        return props;
+    }
+
+    public void runConsume(final Properties consumerProperties) {
+        final String filePath = consumerProperties.getProperty("input.topic.name");
         consumer.subscribe(Collections.singletonList("statusAndLocations"));
-
-        final String filePath = "";
-
         try {
-
             while (true) {
                 ConsumerRecords records = consumer.poll(Duration.ofSeconds(1));
-                FileWritingRecordsHandler handler = new FileWritingRecordsHandler(Paths.get(filePath));
-
-                handler.process(records);
+                recordsHandler.process(records);
             }
         } finally {
             consumer.close();
         }
+    }
+
+    public static void main(String[] args) throws Exception {
+        final Properties properties = StatusAndLocationConsumer.loadProperties("configuration/consumer.properties");
+        final Consumer<String, String> consumer = new KafkaConsumer<>(properties);
+
+        final FileWritingRecordsHandler recordsHandler = new FileWritingRecordsHandler(Paths.get(properties.getProperty("file.path")));
+        final StatusAndLocationConsumer statusAndLocationConsumer = new StatusAndLocationConsumer(consumer, recordsHandler);
+
+        statusAndLocationConsumer.runConsume(properties);
     }
 }
--- src/main/java/StatusAndLocationProducer.java
+++ src/main/java/StatusAndLocationProducer.java
@@ -1,16 +1,20 @@
+import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.Producer;
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.clients.producer.RecordMetadata;
 
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Collection;
 import java.util.List;
+import java.util.Properties;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
 import java.util.stream.Collectors;
 
 public class StatusAndLocationProducer {
-
     private final Producer<String, String> producer;
     final String outTopic;
 
@@ -19,31 +23,68 @@ public class StatusAndLocationProducer {
         outTopic = topic;
     }
 
+    public static Properties loadProperties(String filename) throws IOException {
+        final Properties props = new Properties();
+        final FileInputStream input = new FileInputStream(filename);
+
+        props.load(input);
+        input.close();
+
+        return props;
+    }
+
     // Given a status message we produce a record
     public Future<RecordMetadata> produce(final String message) {
-        final String key = "123";
-        final String value;
-
-        // parse the key and
-
-        final ProducerRecord<String, String> producerRecord = new ProducerRecord<>(outTopic, key);
-
+        final String[] parts = message.split("-");
+        final String key, value;
+        if (parts.length > 1) {
+            key = parts[0];
+            value = parts[1];
+        } else {
+            key = "NO-KEY";
+            value = parts[0];
+        }
+        final ProducerRecord<String, String> producerRecord = new ProducerRecord<>(outTopic, key, value);
         return producer.send(producerRecord);
     }
 
-    public static void main(String[] args) {
-        final Producer<String, String> producer = new StatusAndLocationProducer();
+    // Print the metadata of the record
+    public void printMetadata(final Collection<Future<RecordMetadata>> metadata,
+                              final String fileName) {
+        System.out.println("Offsets and timestamps committed in batch from " + fileName);
+        metadata.forEach(m -> {
+            try {
+                final RecordMetadata recordMetadata = m.get();
+                System.out.println("Record written to offset " + recordMetadata.offset() + " timestamp " + recordMetadata.timestamp());
+            } catch (InterruptedException | ExecutionException e) {
+                if (e instanceof InterruptedException) {
+                    Thread.currentThread().interrupt();
+                }
+            }
+        });
+    }
 
-        String filePath = "";
+    public static void main(String[] args) throws Exception {
+        final Properties properties = StatusAndLocationProducer.loadProperties("configuration/dev.properties");
+        final Producer<String, String> producer = new KafkaProducer<>(properties);
+        final StatusAndLocationProducer statusAndLocationProducer = new StatusAndLocationProducer(producer, properties.getProperty("output.topic.name"));
+
+        String filePath = "input.txt";
 
         try {
             List<String> linesToProduce = Files.readAllLines(Paths.get(filePath));
+
             List<Future<RecordMetadata>> metadata = linesToProduce.stream()
-                    .map(producer::produce)
+                    .map(statusAndLocationProducer::produce)
                     .collect(Collectors.toList());
+
+            statusAndLocationProducer.printMetadata(metadata, filePath);
 
         } catch (IOException e) {
             System.err.printf("Error reading file %s due to %s %n", filePath, e);
+        }
+        finally {
+            producer.close();
         }
     }
 }
