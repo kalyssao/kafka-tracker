dd243184073985a0d371b6b4f08442b59fd38356
kalyssao <klysssa@icloud.com>
2021-11-24 13:34:32 +0000

    Added JPA Repository support

--- src/main/java/repository/StatusAndLocationRepository.java
+++ src/main/java/repository/StatusAndLocationRepository.java
@@ -1,11 +1,12 @@
 package repository;
 
 import models.StatusAndLocation;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 import java.util.Optional;
 
-public interface StatusAndLocationRepository {
+public interface StatusAndLocationRepository extends JpaRepository {
     List<StatusAndLocation> findAll();
 
     Optional<StatusAndLocation> findById(Long id);
